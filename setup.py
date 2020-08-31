645db75fc9295949f80d07c8beba99369f49516f
AlexMDG <alexmdg@protonmail.com>
2020-08-31 03:47:26 +0200

    minor fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PyXTB_amdg", # Replace with your own username
-    version="0.0.2",
+    version="0.0.3",
     author="AlexMDG",
     author_email="alexmdg@protonmail.com",
     description="python wrapper for xStation json API",
